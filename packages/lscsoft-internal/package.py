d51c9388b7e70ccc1906f8fc027791aa1fa5ccc8
Paul Hopkins <paul.hopkins@ligo.org>
2016-08-02 12:12:19 +0100

    Add dummy packages to lalsuite and lscsoft-internal

--- packages/lscsoft-internal/package.py
+++ packages/lscsoft-internal/package.py
@@ -1,32 +1,33 @@
 from spack import *
+from os.path import dirname
 
 class LscsoftInternal(Package):
     """Description"""
 
-    homepage = "https://wiki.ligo.org/DASWG/LALSuite"
-    url      = "1"
+    homepage = "https://wiki.ligo.org/DASWG"
+    # Empty archive shared between meta-packages
+    url="file://" + join_path(dirname(dirname(dirname(__file__))),
+                              'archives', 'empty.tar.gz')
+    version("0.1", "fbfe7b4acab1f9c5642388313270a616")
 
-    version("1")
+    depends_on("py-dqsegdb", type="run")
+    depends_on("py-ligo-lvalert", type="run")
+    depends_on("libframe+matlab", type="run")
+    depends_on("py-glue", type="run")
+    depends_on("lscsoft-lalsuite+swig_python+octave", type="run")
+    depends_on("ldas-tools+matlab", type="run")
+    depends_on("ligotools", type="run")
+    depends_on("metaio", type="run")
+    depends_on("py-pylal", type="run")
+    depends_on("py-pyrxp", type="run")
+    depends_on("py-python-voeventlib", type="run")
+    depends_on("py-ligo-gracedb", type="run")
+    depends_on("nds2-client+octave+python", type="run")
+    depends_on("ligo-lars", type="run")
+    depends_on("gds+core+crtools+monitors+pygds+services", type="run")
 
-    depends_on("py-dqsegdb")
-    depends_on("py-ligo-lvalert")
-    depends_on("libframe+matlab")
-    depends_on("py-glue")
-    depends_on("lscsoft-lalsuite+swig_python+octave")
-    depends_on("ldas-tools+matlab")
-    depends_on("ligotools")
-    depends_on("metaio")
-    depends_on("py-pylal")
-    depends_on("py-pyrxp")
-    depends_on("py-python-voeventlib")
-    depends_on("py-ligo-gracedb")
-    #depends_on("nds2-client+doc+octave+python")
-    #depends_on("ligo-lars")
-    #depends_on("gds+core+crtools+monitors+pygds+services")
-    
+    # Do not install anything and do not check if anything was installed
     def install(self, spec, prefix):
         pass
-    def do_fetch(self):
-        pass
-    def do_stage(self):
+    def sanity_check_prefix(self):
         pass
--- packages/lscsoft-lalsuite/package.py
+++ packages/lscsoft-lalsuite/package.py
@@ -1,5 +1,6 @@
 from spack import *
 from itertools import product
+from os.path import dirname
 
 class LscsoftLalsuite(Package):
     """LSC Algorithm Library Applications.
@@ -8,9 +9,10 @@ class LscsoftLalsuite(Package):
     """
 
     homepage = "https://wiki.ligo.org/DASWG/LALSuite"
-    url      = "1"
-
-    version("1")
+    # Empty archive shared between meta-packages
+    url="file://" + join_path(dirname(dirname(dirname(__file__))),
+                              'archives', 'empty.tar.gz')
+    version("0.1", "fbfe7b4acab1f9c5642388313270a616")
 
     variant('swig_python', True, 'Generate SWIG bindings for Python')
     variant('octave', False, 'Generate SWIG bindings for Octave')
@@ -19,18 +21,7 @@ class LscsoftLalsuite(Package):
     variant('mpi', False, 'Enable MPI')
     variant('extra', True, 'Include lalsuite-extra')
 
-    extends("python")
-
-    depends_on("fftw+float+long_double")
-    depends_on("gsl")
-    depends_on("py-healpy")
-
-    depends_on("libframe")
-    depends_on("metaio")
-
-    depends_on('lalsuite-extra', when='+extra')
-
-    depends_on("mpi", when="+mpi")
+    depends_on('lalsuite-extra', when='+extra', type="run")
 
     def all_combinations(*names):
         combs = []
@@ -39,28 +30,27 @@ class LscsoftLalsuite(Package):
         return [('', None)] + zip(combs, combs)
 
     for c,d in all_combinations('swig_python', 'octave'):
-        depends_on('lalstochastic' + c, when=d)
-        depends_on('lalframe' + c, when=d)
-        depends_on('lalmetaio' + c, when=d)
-   
+        depends_on('lalstochastic' + c, when=d, type="run")
+        depends_on('lalframe' + c, when=d, type="run")
+        depends_on('lalmetaio' + c, when=d, type="run")
+
     for c,d in all_combinations('swig_python', 'octave', 'fastgsl'):
-        depends_on('lal' + c, when=d)
-        depends_on('lalinspiral' + c, when=d)
-        depends_on('laldetchar' + c, when=d)
-        depends_on('lalburst' + c, when=d)
-        depends_on('lalxml' + c, when=d)
-        
+        depends_on('lal' + c, when=d, type="run")
+        depends_on('lalinspiral' + c, when=d, type="run")
+        depends_on('laldetchar' + c, when=d, type="run")
+        depends_on('lalburst' + c, when=d, type="run")
+        depends_on('lalxml' + c, when=d, type="run")
+
     for c,d in all_combinations('swig_python', 'octave', 'fastgsl', 'openmp'):
-        depends_on('lalsimulation' + c, when=d)
-        depends_on('lalpulsar' + c, when=d)
-        depends_on('lalinference' + c, when=d)
+        depends_on('lalsimulation' + c, when=d, type="run")
+        depends_on('lalpulsar' + c, when=d, type="run")
+        depends_on('lalinference' + c, when=d, type="run")
 
     for c,d in all_combinations('fastgsl', 'openmp', 'mpi'):
-        depends_on('lalapps' + c, when=d)
+        depends_on('lalapps' + c, when=d, type="run")
 
+    # Do not install anything and do not check if anything was installed
     def install(self, spec, prefix):
         pass
-    def do_fetch(self):
-        pass
-    def do_stage(self):
+    def sanity_check_prefix(self):
         pass
