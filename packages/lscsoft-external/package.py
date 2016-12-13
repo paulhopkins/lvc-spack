ff55677bf0570a3de947225f0792114d81c36edc
Paul Hopkins <paul.hopkins@ligo.org>
2016-12-13 13:15:44 +0000

    Add more external dependencies

--- packages/lscsoft-external/package.py
+++ packages/lscsoft-external/package.py
@@ -10,8 +10,16 @@ class LscsoftExternal(Package):
                               'archives', 'empty.tar.gz')
     version("0.1", "fbfe7b4acab1f9c5642388313270a616")
 
-    depends_on("lscsoft-external-python")
-    depends_on("texlive scheme=medium")
+    depends_on("lscsoft-external-python", type='run')
+    depends_on("texlive scheme=medium", type='run')
+    depends_on("git", type='run')
+    depends_on("git-lfs", type='run')
+    depends_on("ImageMagick", type='run')
+    depends_on("duc", type='run')
+    depends_on("environment-modules", type='run')
+    depends_on("root", type='run')
+    depends_on("openblas", type='run')
+    depends_on("mercurial", type='run')
 
     # Do not install anything and do not check if anything was installed
     def install(self, spec, prefix):
