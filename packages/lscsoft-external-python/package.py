58074a8872de039b29ad2855f3a9fe071c844cfa
Paul Hopkins <paul.hopkins@ligo.org>
2016-12-13 13:13:00 +0000

    Add more Python dependencies

--- packages/lscsoft-external-python/package.py
+++ packages/lscsoft-external-python/package.py
@@ -11,22 +11,21 @@ class LscsoftExternalPython(Package):
     version("0.1", "fbfe7b4acab1f9c5642388313270a616")
 
 #    depends_on("GitPython")
-    depends_on("py-h5py")
-    depends_on("py-virtualenv")
-    depends_on("py-virtualenvwrapper")
-    depends_on("py-setuptools")
-    depends_on("py-basemap")
-
-
-#    depends_on("ipython")
-#    depends_on("numpy")
-#    depends_on("py-basemap")
+    depends_on("py-h5py", type='run')
+    depends_on("py-virtualenv", type='run')
+    depends_on("py-virtualenvwrapper", type='run')
+    depends_on("py-setuptools", type='run')
+    depends_on("py-basemap", type='run')
 #    depends_on("py-basemap-data-hires")
 #    depends_on("py-basemap-examples")
-#    depends_on("py-matplotlib")
+    depends_on("py-numpy", type='run')
+    depends_on("py-matplotlib", type='run')
+    depends_on("py-scipy", type='run')
+    depends_on("py-setuptools", type='run')
+    depends_on("py-ipython", type='run')
+    depends_on("py-shapely", type='run')
 #    depends_on("py-pip")
 #    depends_on("py-sqlobject")
-#    depends_on("py-virtualenv")
 #    depends_on("py34")
 #    depends_on("py34-crypto")
 #    depends_on("py34-devel")
@@ -36,7 +35,7 @@ class LscsoftExternalPython(Package):
 #    depends_on("py34-numpy-f2py")
 #    depends_on("py34-setuptools")
 #    depends_on("py34-setuptools_scm")
-#    depends_on("scipy")
+
 
     # Do not install anything and do not check if anything was installed
     def install(self, spec, prefix):
