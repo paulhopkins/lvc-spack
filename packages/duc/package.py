970822fc8557682960c425aaa50fe0aaadbc63a9
Paul Hopkins <paul.hopkins@ligo.org>
2017-01-13 15:16:56 +0000

    duc: Add version 1.4.2 and 1.4.3

--- packages/duc/package.py
+++ packages/duc/package.py
@@ -10,6 +10,8 @@ class Duc(Package):
     homepage = "http://duc.zevv.nl/"
     url      = "https://github.com/zevv/duc/releases/download/1.4.1/duc-1.4.1.tar.gz"
 
+    version('1.4.3', 'c67999bf8a5668e8a78a8b3abe8dba9f')
+    version('1.4.2', 'f55c0d132cd46e15bafa7a1086e8658c')
     version('1.4.1', 'c2108ef00f20784a9401ae95550adf2e')
 
     depends_on('sqlite')
